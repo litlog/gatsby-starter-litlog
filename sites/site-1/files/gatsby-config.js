57e2de41f455477ffb0e872ae53a51527b51753e
Carl W. Hunt <cwhunt@litlog.io>
2018-07-30 15:50:52 -0700

    configured for deploy

--- sites/site-1/files/gatsby-config.js
+++ sites/site-1/files/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Litlog Starter',
   },
   plugins: ['gatsby-plugin-react-helmet'],
+  pathPrefix: '/gatsby-starter-litlog'
 }
