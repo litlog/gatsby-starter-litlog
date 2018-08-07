0febfa97c83c623b638ff1661206bdbc50004216
Carl W. Hunt <cwhunt@litlog.io>
2018-08-07 10:41:40 -0700

    added task-1 plan step-10

--- sites/common/files/src/templates/litlog-page.js
+++ sites/common/files/src/templates/litlog-page.js
@@ -1,14 +1,24 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import rehypeReact from "rehype-react"
+
 import Layout from "../components/layout"
 
 export default ({ data }) => {
   const post = data.markdownRemark
+  const renderAst = new rehypeReact({
+    createElement: React.createElement,
+    components: {
+      "g-link": Link
+    },
+  }).Compiler;
   return (
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        {
+          renderAst(post.htmlAst)
+        }
       </div>
     </Layout>
   )
@@ -16,8 +26,8 @@ export default ({ data }) => {
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq:  $slug } }) {
-      html
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      htmlAst
       frontmatter {
         title
       }
